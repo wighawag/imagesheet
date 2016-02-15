1ad955d0cec555b20442190ac6914295b06955f6
wighawag <wighawag@gmail.com>
2016-02-15 22:21:52 +0000

    working

--- Sources/imagesheet/ImageSheet.hx
+++ Sources/imagesheet/ImageSheet.hx
@@ -1,16 +1,30 @@
 package imagesheet;
 
 import haxe.Json;
+import kha.Image;
+import kha.Assets;
 
 class SubImage{
-	public var x1(default,null) : Int;
-	public var y1(default,null) : Int;
-	public var x2(default,null) : Int;
-	public var y2(default,null) : Int;
-	public var refX(default,null) : Int;
-	public var refY(default,null) : Int;
-	private function new(){
-		
+	public var x(default,null) : Int;
+	public var y(default,null) : Int;
+	public var width(default,null) : Int;
+	public var height(default,null) : Int;
+	public var offsetX(default,null) : Int;
+	public var offsetY(default,null) : Int;
+	public var originalWidth(default,null) : Int;
+	public var originalHeight(default,null) : Int;
+	public var rotated(default,null) : Bool;
+	
+	private function new(x : Int, y : Int, width : Int, height : Int, offsetX : Int, offsetY : Int, originalWidth : Int, originalHeight : Int, rotated : Bool){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.originalWidth = originalWidth;
+		this.originalHeight = originalHeight;
+		this.rotated = rotated;
 	}
 }
 
@@ -19,13 +33,15 @@ class ImageSheet{
 	public static function fromTexturePackerJsonArray(jsonString : String) : ImageSheet{
 		var json = Json.parse(jsonString);
 		var meta = json.meta;
-		var frames = json.frames;
+		//TODO better kha handling of dynamic asset path:
+		var image = Reflect.field(Assets.images, meta.image.substr(0,meta.image.indexOf(".")));
+		var frames : Array<Dynamic> = json.frames;
 		var subImages : Map<String, SubImage> = new Map();
 		for(frame in frames){
-			var subImage = new SubImage();
+			var subImage = new SubImage(frame.frame.x, frame.frame.y, frame.frame.w, frame.frame.h, frame.spriteSourceSize.x, frame.spriteSourceSize.y, frame.sourceSize.w, frame.sourceSize.h,  frame.rotated);
 			subImages.set(frame.filename, subImage);
 		}
-		return new ImageSheet(subImages);
+		return new ImageSheet(subImages, image);
 	}
 	
 	public var image(default, null) : Image;
@@ -35,8 +51,9 @@ class ImageSheet{
 	}
 	
 	private var subImages : Map<String, SubImage>;
-	private function new(subImages : Map<String, SubImage>){
+	private function new(subImages : Map<String, SubImage>, image : Image){
 		this.subImages = subImages;
+		this.image = image;
 	}
 	
 	
